198b24c58f7bd8e576a5d7d8adc118c0ced1c503
Muhammad Ansar <57707946+embeddedlab786@users.noreply.github.com>
2021-03-14 15:37:14 -0700

    Update Wifi_4Load_AP_Web_Server.ino

--- Wifi_4Load_AP_Web_Server.ino
+++ Wifi_4Load_AP_Web_Server.ino
@@ -114,7 +114,7 @@ void handle_NotFound(){
 String SendHTML(uint8_t l1stat,uint8_t l2stat,uint8_t l3stat,uint8_t l4stat){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>LED Control</title>\n";
+  ptr +="<title>4Load Control</title>\n";
   ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
   ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
   ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
